e2a74c11969698f2f9f8dd60513015fb763fd217
gatere <mercynjerigatere@gmail.com>
2024-04-03 06:17:12 -0400

    Adds arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
 
-// Write your code here
+/*function multiply(num1 = 2, num2 = 31) {
+    return num1*num2
+}
+
+function random(){
+    return Math.round((Math.random())*10)
+}
+
+function mod(num3 = 16, num4 = 6) {
+    let raw = num3 / num4
+    let refined = Math.floor (raw)
+    let rem = raw - refined
+    return rem
+}
+
+function max (array) {
+ return Math.max(array)
+}
+let gif = [3,4,5,26,9,5]
+console.log(max(gif));*/
+
+let num1 = 2;
+let num2 = 31;
+let num3 = 24;
+let num4 = 5;
+const multiply = (num1*num2);
+const random = (Math.floor(Math.random()*10)+1);
+const mod = (num3 % num4);
+const max = (Math.max(1,2,3,4,20));
+
+
